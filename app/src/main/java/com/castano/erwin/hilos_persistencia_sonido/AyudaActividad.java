45ebea1aeb443c5b9389296637b33d650f68efe2
Erwin Felipe Castano Mora <ecastano@bancolombia.com.co>
2017-12-20 08:20:07 -0500

    se agregan xml segunda actividad

---
+++ app/src/main/java/com/castano/erwin/hilos_persistencia_sonido/AyudaActividad.java
@@ -0,0 +1,13 @@
+package com.castano.erwin.hilos_persistencia_sonido;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AyudaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda_actividad);
+    }
+}
--- app/src/main/java/com/castano/erwin/hilos_persistencia_sonido/MainActivity.java
+++ app/src/main/java/com/castano/erwin/hilos_persistencia_sonido/MainActivity.java
@@ -1,7 +1,9 @@
 package com.castano.erwin.hilos_persistencia_sonido;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void ayuda(View vista){
+        Intent intencion = new Intent(this, AyudaActividad.class);
+        startActivity(intencion);
+
     }
 }
